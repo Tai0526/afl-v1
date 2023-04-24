28fd3ca9248fd373fa2a706f0855b4b17678e782
Taizya Kasitu <taizyakasitu2001@gmail.com>
2023-04-24 15:27:25 +0100

    updated index

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Hero from "@/components/Hero";
-import Categories from "@/components/categories";
 
 const inter = Inter({ subsets: ["latin"] });
 
